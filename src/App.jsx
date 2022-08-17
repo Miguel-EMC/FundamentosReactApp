ad1408d0c9c47aaf522fa8a0d491b1a4a9815420
Miguel-EMC <eduardomuzo123456@gmail.com>
2022-08-16 23:29:59 -0500

    1.1 LIMPIEZA DEL PROYECTO

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import {  BrowserRouter,  Routes,  Route } from "react-router-dom";
+import Dashboard from "./layout/Dashboard";
+import Login from "./layout/Login";
+import FormularioLogin from "./pages/FormularioLogin";
+import FundamentoUseEffect from "./pages/FundamentoUseEffect";
+import FundamentoUseState from "./pages/FundamentoUseState";
+import LandingPage from "./pages/LandingPage";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<LandingPage />}>
+        </Route>
+
+        <Route path="/login" element={<Login />}>
+          <Route index element={<FormularioLogin/>}/>
+        </Route>
+
+        <Route path="/fundamentos" element={<Dashboard />}>
+          <Route path="usestate" element={<FundamentoUseState />} />
+          <Route path="useffect" element={<FundamentoUseEffect />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
