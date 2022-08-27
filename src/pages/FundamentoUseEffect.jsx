fe23ecbb3e9e3462f4ee7a3a04e5a09c87f19e94
Miguel-EMC <eduardomuzo123456@gmail.com>
2022-08-27 15:51:27 -0500

    1.2 USE STATE

--- src/pages/FundamentoUseEffect.jsx
+++ src/pages/FundamentoUseEffect.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import FetchUsers from '../components/effect/FetchUsers'
 import Intro from '../components/effect/Intro'
+import IntroUseEffect from '../components/effect/IntroUseEffect'
+import Time from '../components/effect/Time'
 
 const FundamentoUseEffect = () => {
   return (
-    <Intro/>
+    // <Intro/>
+    // <IntroUseEffect/>
+     //<Time/>
+    <FetchUsers/>
   )
 }
 
